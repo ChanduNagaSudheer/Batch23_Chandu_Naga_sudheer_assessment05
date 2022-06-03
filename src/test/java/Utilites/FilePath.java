76286fdb3a6cd243da2e7ed714b1aa3361bd2428
775661 <chandunagasudheer215@gmail.com>
2022-06-03 21:02:04 +0530

    final commit

---
+++ src/test/java/Utilites/FilePath.java
@@ -0,0 +1,11 @@
+package Utilites;
+
+public interface FilePath {
+
+	
+	public static String ChromePath ="C:\\Users\\gorrela\\Desktop\\sdet\\automation assignments\\Batch23_Chandu_Naga_Sudheer_assessment05-main\\src\\test\\resources\\Drivers\\chromedriver.exe";
+	public static String PropertiesPath ="C:\\Users\\gorrela\\Desktop\\sdet\\automation assignments\\Batch23_Chandu_Naga_Sudheer_assessment05-main\\src\\test\\resources\\ConfigFiles\\Config.properties";
+	public static String XLPath ="C:\\Users\\gorrela\\Desktop\\sdet\\automation assignments\\Batch23_Chandu_Naga_Sudheer_assessment05-main\\src\\test\\resources\\ExcelFiles\\Testxl.xlsx";
+	
+
+}
